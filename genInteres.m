a5e4fdcf347a9fe953ae91f7d31579aad9772fd6
Allen Tung <Snowrries@users.noreply.github.com>
2017-03-16 18:44:30 -0400

    Function in matlab to create an interest point

---
+++ genInteres.m
@@ -0,0 +1,12 @@
+function [F] = genInteres(a)
+mu = [0 0];
+Sigma = [.25 .3; .3 1];
+x1 = -a:.2:a; x2 = -a:.2:a;
+[X1,X2] = meshgrid(x1,x2);
+F = mvnpdf([X1(:) X2(:)],mu,Sigma);
+F = reshape(F,length(x2),length(x1)); 
+F = F ./max(max(F));
+surf(x1,x2,F);
+caxis([min(F(:))-.5*range(F(:)),max(F(:))]);
+axis([-3 3 -3 3 0 1])
+xlabel('x1'); ylabel('x2'); zlabel('Probability Density');
