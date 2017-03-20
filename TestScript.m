878af046e611d21079d7b53327c3e00365710c86
aw449 <awong7905@gmail.com>
2017-03-20 15:25:42 -0400

    Added Gradient Graphs
    Added Gradient Graphs to aid in visualization in determining what
    algorithm to use.

--- TestScript.m
+++ TestScript.m
@@ -10,6 +10,7 @@ axis([0 100 0 100 0 1])
 xlabel('x1'); ylabel('x2'); zlabel('Probability Density');
 hold on;
 
+%% Phase 1 Test - Sparse Traversal Function
 %Init class
 BlueRov = AUV;
 BlueRov.sparseTraverse(10,world,'E','S')
@@ -17,3 +18,18 @@ z = ones(size(BlueRov.previous_x)) * 0.3;
 plot3(BlueRov.previous_x, BlueRov.previous_y,z,'r');
 hold off;
 
+
+%% Phase 2 Test - Marking pollution levels
+figure
+[px,py] = gradient(world);
+quiver(px,py);
+title('World Gradients');
+xlim([0,100]);
+ylim([0,100]);
+
+figure
+[ppx,ppy] = gradient(BlueRov.current_knowledge);
+quiver(ppx,ppy);
+title('Sparse Traversal Gradients');
+xlim([0,100]);
+ylim([0,100]);
