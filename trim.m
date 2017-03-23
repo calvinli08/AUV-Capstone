d0b57df74a63f6ab40e899dc6ff403682d70ddf3
Allen <allentung5@gmail.com>
2017-03-23 04:48:22 -0400

    trim

---
+++ trim.m
@@ -0,0 +1,26 @@
+%poi 
+function trim = trim(poi, thresh)
+poi = [1 1; 2 2; 3 3; 4 4; 10 12; 13 14; 22 1; 1 30; 54 54; 600 1];
+
+thresh = 2;
+    a = size(poi);
+    a = a(1); %a is now the number of rows there are in poi.
+    i = 1;
+    while i < a
+        j = i+1;
+        while j < a
+            if(pdist([poi(i,:);poi(j,:)]) < thresh)
+                if( i > 1 ) 
+                    poi = [poi(1:i-1, :); poi(i+1 : a)];
+                else
+                    poi = poi(2:a,:);
+                end
+                a = a-1;
+            else
+                j = j+1;
+            end
+        end
+        i = i+1;
+    end
+    trim = poi;
+end
