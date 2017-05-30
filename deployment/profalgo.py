f6f4f32d4d46a1302252ace8ae07d5bb1e4d8af3
Calvin Li <cl837@scarletmail.rutgers.edu>
2017-05-30 18:11:40 -0400

    Defined the __INIT__, SURFACE, BEARINGCHECK, and UPDATE functions. will finish these functions and also add TRAVERSE, ERRORCHECK, and GPS functions, and other functions as necessary.

--- deployment/profalgo.py
+++ deployment/profalgo.py
@@ -11,9 +11,35 @@ import mavutils as mav
 import os
 
 class AUV:
+    #__init__
+    def __init__(self):
+        log = open('''path to log''',"r")
+        self.battery = log.readline()
+        self.bearing = log.readline()
+        self.velocity = log.readline()
+        self.magX =
+        self.magY =
+        self.magZ =
+        self.time =
+
+    #updates all the navigational parameters. Essentially __init__.
+    def update(self):
+        log = open('''path to log''',"r")
+        self.battery = log.readline()
+        self.bearing = log.readline()
+        self.velocity = log.readline()
+        self.magX =
+        self.magY =
+        self.magZ =
+        self.time =
+
     #Determines whether to surface
-    def surface(battery, bearing, time):
-        if battery <= 0.2 or time >= 180:
+    def surface(self, battery, bearing, time, velocity):
+        #BATTERY is measured as a percentage
+        #TIME is in minutes
+        #VELOCITY is in meters/second
+        self.update(self)
+        if self.battery <= 0.2 or self.time >= 180 or self.velocity < :
             mav.#surface command, spin vertical motors
             return false
         else:
